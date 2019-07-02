c60c256a7e167fc0e4af5bdd70376c9c2c5ab87a
Marcelo Silva <marcelosilva.developer@gmail.com>
2019-07-01 22:58:31 -0300

    Add copyright and align content

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import Editor from './components/Editor';
 
 const Wrapper = styled.section`
-  margin: 5rem auto 0 auto;
+  margin: 0 auto;
   width: 672px;
   display: flex;
   flex-direction: column;
@@ -16,10 +16,37 @@ const Title = styled.h1`
   color: ${props => props.theme.colors.primary_color};
 `;
 
+const Copyright = styled.span`
+  margin-top: 1rem;
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: 400;
+  color: ${props => props.theme.colors.gray_high};
+`;
+
+const Heart = styled.span`
+  display: inline-block;
+  font-size: 1.2rem;
+  transform: scale(1);
+  color: ${props => props.theme.colors.primary_color_low};
+  animation: love 0.5s infinite linear alternate-reverse;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: ${props => props.theme.colors.primary_color_low};
+`;
+
 const Bootstrap = () => (
   <Wrapper>
     <Title>Rich text editing with Draft.js</Title>
     <Editor />
+    <Copyright>
+      Made with <Heart>♥</Heart> by{' '}
+      <Link href="https://github.com/iamtchelo" target="_blank">
+        Marcelo Silva
+      </Link>
+    </Copyright>
   </Wrapper>
 );
 
